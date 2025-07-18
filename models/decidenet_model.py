07c32890143a601848d0f428f4d4fdbe9c02fd4a
Myles Lim <myleslimwl@gmail.com>
2025-07-19 03:17:11 +0800

    updated decidenet_model.py

--- models/decidenet_model.py
+++ models/decidenet_model.py
@@ -1,81 +1,32 @@
 import torch
 import torch.nn as nn
-import torchvision.models as models
 from torchvision.models.detection import fasterrcnn_resnet50_fpn
+from models.modules import RegressionBranch, AttentionBranch
 
-
-class RegressionBranch(nn.Module):
+class DecideNet(nn.Module):
     def __init__(self):
-        super(RegressionBranch, self).__init__()
-        resnet = models.resnet50(weights=None)
-        self.backbone = nn.Sequential(*list(resnet.children())[:-2])  # remove avgpool & fc
+        super(DecideNet, self).__init__()
+        self.reg_branch = RegressionBranch()
+        self.det_branch = DetectionBranch()
+        self.att_branch = AttentionBranch()
 
-        self.decoder = nn.Sequential(
-            nn.Conv2d(2048, 512, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 128, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(128, 1, 1)  # output density map
-        )
+    def forward(self, images, targets=None):
+        reg_output = self.reg_branch(images)
+        det_output = self.det_branch(images, targets)
+        attention_map = self.att_branch(images)
 
-    def forward(self, x):
-        features = self.backbone(x)
-        density_map = self.decoder(features)
-        return density_map
-
-
-class AttentionBranch(nn.Module):
-    def __init__(self):
-        super(AttentionBranch, self).__init__()
-        self.attention = nn.Sequential(
-            nn.Conv2d(2, 1, kernel_size=1),
-            nn.Sigmoid()
-        )
-
-    def forward(self, reg_map, det_map):
-        x = torch.cat([reg_map, det_map], dim=1)
-        attention = self.attention(x)
-        return attention
+        final_output = attention_map * det_output + (1 - attention_map) * reg_output
+        return final_output, reg_output, det_output, attention_map
 
 
 class DetectionBranch(nn.Module):
     def __init__(self):
         super(DetectionBranch, self).__init__()
         self.detector = fasterrcnn_resnet50_fpn(pretrained=True)
-        self.detector.train()
+        in_features = self.detector.roi_heads.box_predictor.cls_score.in_features
+        self.detector.roi_heads.box_predictor = nn.Sequential(
+            nn.Linear(in_features, 2)  # 1 class + background
+        )
 
     def forward(self, images, targets=None):
-        if self.training:
-            return self.detector(images, targets)
-        else:
-            return self.detector(images)
-
-
-class DecideNet(nn.Module):
-    def __init__(self):
-        super(DecideNet, self).__init__()
-        self.reg_branch = RegressionBranch()
-        self.att_branch = AttentionBranch()
-        self.det_branch = DetectionBranch()
-
-    def forward(self, images, targets=None):
-        # Regression output
-        reg_output = self.reg_branch(images)
-
-        # Detection output: expects list of dicts if training
-        if self.training and targets is not None:
-            det_output = self.det_branch(images, targets)
-        else:
-            det_output = self.det_branch(images)
-
-        # Create a detection density map (simulated here)
-        # Note: You should define a function to convert detection boxes to heatmaps
-        det_density_map = torch.zeros_like(reg_output)
-
-        # Attention output
-        att_map = self.att_branch(reg_output, det_density_map)
-
-        # Final output using attention map
-        final_output = att_map * reg_output + (1 - att_map) * det_density_map
-
-        return final_output, reg_output, det_output, att_map
+        return self.detector(images, targets)
